4fa2d6e16f2ed28c917b818f0b36f74753390c45
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-05 16:22:57 -0300

    Feat: Adiciona manipulação com foreach.

---
+++ foreach/foreach-01.js
@@ -0,0 +1,49 @@
+const list = [
+  { name: 'andré',
+    age: 30,
+  },
+  { name: 'joão',
+    age: 20,
+  },
+  { name: 'maria',
+    age: 25,
+  },
+  { name: 'josé',
+    age: 40,
+  }
+]
+
+// for(let i = 0; i < list.length; i++) {
+  // console.log(list[i].name)
+  // console.log(list[i])
+// }
+
+// for(const item of list) {
+  // console.log(item.name)
+  // console.log(item)
+// }
+
+
+// list.forEach(item => {
+//   console.log(item.name)
+//   console.log(item)
+// })
+
+let sumAgeA = 0;
+
+list.forEach((item) => {
+  sumAgeA += item.age
+})
+console.log(sumAgeA)
+
+
+let sumAgeB = 0;
+
+const sumAgeFunction = (item) => {
+  sumAgeB += item.age
+}
+
+list.forEach(sumAgeFunction)
+
+console.log(sumAgeB)
+
